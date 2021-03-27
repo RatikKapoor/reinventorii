2bd04ac9ed82cc8ffe70e9d0a802b98903afb749
Robert Brown <brown.rjb75@gmail.com>
2021-03-27 12:36:38 -0600

    test: added basic unit test

---
+++ edu/ucalgary/ensf409/DatabaseTest.java
@@ -0,0 +1,31 @@
+package edu.ucalgary.ensf409;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.*;
+
+public class DatabaseTest {
+    @Test
+    public void testDBConnect() {
+        Database testDb = new Database("jdbc:mysql://server.ratik.me:25565/db", "ensf409", "java123");
+        try {
+            testDb.connect();
+        } catch (SQLException e) {
+            fail("Err");
+        }
+    }
+
+    @Test
+    public void testFailDBConnect() {
+        Database testDb = new Database("jdbc:mysql://server.ratik.me:12345/db", "ensf409", "java123");
+        Boolean err = false;
+        try {
+            testDb.connect();
+        } catch (SQLException e) {
+            err = true;
+        }
+        assertTrue(err);
+    }
+}
