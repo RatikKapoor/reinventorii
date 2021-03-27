a7dc3c0b3bbf93bafff0b9685531b5ed6f66e01b
Robert Brown <brown.rjb75@gmail.com>
2021-03-27 15:40:34 -0600

    refactor!: added springboot

---
+++ src/test/java/edu/ucalgary/ensf409/ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ucalgary.ensf409;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/edu/ucalgary/ensf409/DatabaseTest.java
@@ -0,0 +1,30 @@
+package edu.ucalgary.ensf409;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+public class DatabaseTest {
+    @Test
+    public void testDBConnect() {
+        Database testDb = new Database("jdbc:mysql://server.ratik.me:25565/db", "ensf409", "java123");
+        try {
+            testDb.connect();
+        } catch (SQLException e) {
+            fail("Err");
+        }
+    }
+
+    @Test
+    public void testFailDBConnect() {
+        Database testDb = new Database("jdbc:mysql://server.ratik.me:12345/db", "ensf409", "java123");
+        Boolean err = false;
+        try {
+            testDb.connect();
+        } catch (SQLException e) {
+            err = true;
+        }
+        assertTrue(err);
+    }
+}
