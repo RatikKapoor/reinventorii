e1e6bd485e7cb3945c1f47f6aa6ab07c4c8a2ef8
Robert Brown <brown.rjb75@gmail.com>
2021-03-27 15:40:34 -0600

    refactor!: added springboot

---
+++ src/main/java/edu/ucalgary/ensf409/Greeting.java
@@ -0,0 +1,24 @@
+package edu.ucalgary.ensf409;
+
+/**
+ * code from https://spring.io/guides/gs/rest-service/
+ */
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+}
