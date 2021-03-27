24d9cfde83c40c604f85e48ffd803de40a47b934
Robert Brown <brown.rjb75@gmail.com>
2021-03-27 12:08:02 -0600

    chroe: removed jar files from .gitignore

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        Database test = new Database("jdbc:mysql://server.ratik.me:25565/db", "ensf409", "java123");
+        test.connect();
+    }
+}
---
+++ src/Database.java
@@ -0,0 +1,38 @@
+import java.sql.*;
+
+/**
+ * a class for connecting to the Database
+ * @author Robert Brown
+ */
+public class Database {
+    public final String DBURL;
+    public final String USERNAME;
+    public final String PASSWORD;
+
+    private Connection dbConnect;
+    private ResultSet results;
+
+    /**
+     * constructor fo the database class
+     * @param DBURL url of the database
+     * @param USERNAME  username for access to the database
+     * @param PASSWORD  password for access to the database
+     */
+    public Database(String DBURL, String USERNAME, String PASSWORD) {
+        this.DBURL = DBURL;
+        this.USERNAME = USERNAME;
+        this.PASSWORD = PASSWORD;
+    }
+
+    /**
+     * a method to connect to the database
+     */
+    public void connect() {
+        try {
+            dbConnect = DriverManager.getConnection(DBURL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+    }
+
+}
