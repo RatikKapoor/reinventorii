c30a217066c93ba550f11d5d5930862581b3ee8d
Robert Brown <brown.rjb75@gmail.com>
2021-03-27 13:17:47 -0600

    feat: added a basic connection to the database

--- edu/ucalgary/ensf409/App.java
+++ edu/ucalgary/ensf409/App.java
@@ -3,7 +3,8 @@ package edu.ucalgary.ensf409;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        Database test = new Database("jdbc:mysql://server.ratik.me:25565/db", "ensf409", "java123");
+        Database test = new Database("jdbc:mysql://server.ratik.me:25565/INVENTORY", "root", "eNsF409");
         test.connect();
+        test.getManufacturers();
     }
 }
