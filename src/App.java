e4b915966ddfbf668515491a1b20f9a014afd468
Robert Brown <brown.rjb75@gmail.com>
2021-03-27 12:15:53 -0600

    fix: removed src folder

--- src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Database test = new Database("jdbc:mysql://server.ratik.me:25565/db", "ensf409", "java123");
-        test.connect();
-    }
-}
