65068ec960614210027f27fc94e82bbdd1ed6323
Robert Brown <brown.rjb75@gmail.com>
2021-03-27 11:55:30 -0600

    feat: connected to db

--- edu/ucalgary/ensf409/App.java
+++ edu/ucalgary/ensf409/App.java
@@ -3,5 +3,7 @@ package edu.ucalgary.ensf409;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Database test = new Database("jdbc:mysql://server.ratik.me:25565/db", "ensf409", "java123");
+        test.connect();
     }
 }
