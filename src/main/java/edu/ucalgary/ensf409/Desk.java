505239b9ab5bffa6834b66dc6f618eb3e14d291c
Risat <risathq@gmail.colm>
2021-03-27 20:19:39 -0600

    Added Desk again

---
+++ src/main/java/edu/ucalgary/ensf409/Desk.java
@@ -0,0 +1,55 @@
+package edu.ucalgary.ensf409;
+
+/**
+ * Implementation of class Chair extending from FurniturePart
+ */
+public class Desk extends FurniturePart {
+    private boolean legs;
+    private boolean top;
+    private boolean drawer;
+
+    /**
+     * Constructor for Chair
+     * 
+     * @param id
+     * @param type
+     * @param legs
+     * @param top
+     * @param drawer
+     * @param price
+     * @param manuID
+     */
+    public Desk(String id, String type, String legs, String top, String drawer, int price, String manuID) {
+
+        super(id, type, price, manuID);
+        this.legs = stringToBoolean(legs);
+        this.top = stringToBoolean(top);
+        this.drawer = stringToBoolean(drawer);
+
+    }
+
+    public Boolean getLegs() {
+        return this.legs;
+    }
+
+    public void setLegs(Boolean legs) {
+        this.legs = legs;
+    }
+
+    public Boolean getTop() {
+        return this.top;
+    }
+
+    public void setTop(Boolean top) {
+        this.top = top;
+    }
+
+    public Boolean getDrawer() {
+        return this.drawer;
+    }
+
+    public void setDrawer(Boolean drawer) {
+        this.drawer = drawer;
+    }
+
+}
