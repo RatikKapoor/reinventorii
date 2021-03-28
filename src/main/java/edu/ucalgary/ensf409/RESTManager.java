1ce4a76fc03e8b1ae331dbfcd8f2db5443c45635
Ratik Kapoor <ratikrk@gmail.com>
2021-03-27 23:13:52 -0600

    feat: Added RESTManager

---
+++ src/main/java/edu/ucalgary/ensf409/RESTManager.java
@@ -0,0 +1,43 @@
+package edu.ucalgary.ensf409;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.ucalgary.ensf409.FurniturePart.Types;
+
+@RestController
+public class RESTManager {
+    Database database = null;
+
+    public RESTManager() {
+        database = new Database("jdbc:mysql://server.ratik.me:25565/INVENTORY", "root", "eNsF409");
+        try {
+            database.connect();
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+    }
+
+    @GetMapping("/chairs")
+    public ArrayList<Chair> chairs(@RequestParam(value = "type", defaultValue = "") String type) {
+        return type.length() == 0 ? database.getList(Types.Chair) : database.getListByType(Types.Chair, type);
+    }
+
+    @GetMapping("/desks")
+    public ArrayList<Chair> desk(@RequestParam(value = "type", defaultValue = "") String type) {
+        return type.length() == 0 ? database.getList(Types.Desk) : database.getListByType(Types.Desk, type);
+    }
+
+    @GetMapping("/filings")
+    public ArrayList<Chair> filing(@RequestParam(value = "type", defaultValue = "") String type) {
+        return type.length() == 0 ? database.getList(Types.Filing) : database.getListByType(Types.Filing, type);
+    }
+
+    @GetMapping("/lamps")
+    public ArrayList<Chair> lamps(@RequestParam(value = "type", defaultValue = "") String type) {
+        return type.length() == 0 ? database.getList(Types.Lamp) : database.getListByType(Types.Lamp, type);
+    }
+}
