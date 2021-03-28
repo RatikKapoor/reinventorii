c0284a912282b2a0e43725b2e07fda5f2350c176
Ratik Kapoor <ratikrk@gmail.com>
2021-03-27 21:38:46 -0600

    feat: Added FurnitureParts.Types enum

--- src/main/java/edu/ucalgary/ensf409/FurniturePart.java
+++ src/main/java/edu/ucalgary/ensf409/FurniturePart.java
@@ -1,11 +1,33 @@
 package edu.ucalgary.ensf409;
 
+import java.util.Arrays;
+import java.util.List;
+
 public abstract class FurniturePart {
+    public enum Types {
+        Chair(new String[] { "Legs", "Arms", "Seat", "Cushion" }), Desk(new String[] { "Legs", "Top", "Drawer" }),
+        Filing(new String[] { "Rails", "Drawers", "Cabinet" }), Lamp(new String[] { "Base", "Bulb" });
+
+        private final List<String> properties;
+
+        Types(String[] properties) {
+            this.properties = Arrays.asList(properties);
+        }
+
+        public boolean hasProperty(String property) {
+            return this.properties.contains(property);
+        }
+
+        @Override
+        public String toString() {
+            return this.name();
+        }
+    }
+
     private String id;
     private String type;
     private int price;
     private String ManuID;
-    public String[] params;
 
     public String getId() {
         return this.id;
@@ -43,12 +65,11 @@ public abstract class FurniturePart {
         this.type = type;
     }
 
-    public FurniturePart(String id, String type, int price, String ManuID, String[] params) {
+    public FurniturePart(String id, String type, int price, String ManuID) {
         this.id = id;
         this.type = type;
         this.price = price;
         this.ManuID = ManuID;
-        this.params = params;
     }
 
     /**
