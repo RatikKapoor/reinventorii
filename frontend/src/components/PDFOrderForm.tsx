a6972804e3ab1f9ba70a1d02a4e2cf8eb7276727
Robert Brown <brown.rjb75@gmail.com>
2021-04-11 22:15:35 -0600

    feat: updated pdf style - closes #52

--- frontend/src/components/PDFOrderForm.tsx
+++ frontend/src/components/PDFOrderForm.tsx
@@ -12,33 +12,41 @@ import { GiOfficeChair } from "react-icons/gi";
 import { IonButton } from "@ionic/react";
 import { Furniture } from "../interfaces/FurnitureTypes";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  defaultMargin: {marginLeft: 50, marginRight: 50},
+  titleBar: {marginLeft: 50, marginRight: 50, marginTop: 75, marginBottom: 40},
+  text: {color: '#444444'},
+  title: {color: '#444444', fontSize: 50},
+  contactInfo: {backgroundColor: '#DEDEDE', paddingVertical: 20, paddingHorizontal: 20},
+  finalPrice: {fontSize: 25, fontWeight: 'bold'},
+  finalPriceContainer: {backgroundColor: '#DEDEDE', marginTop: 10, paddingVertical: 20, paddingHorizontal: 20}
+});
 
 const MyDocument = (props: OrderViewProps) => (
   <Document>
     <Page size="Letter">
-      <View>
-        <Text>Furniture Order Form</Text>
+      <View style={styles.titleBar}>
+        <Text style={styles.title}>Furniture Order Form</Text>
       </View>
-      <View>
-        <Text>Faculty Name: {props.faculty}</Text>
-        <Text>Contact: {props.contact}</Text>
-        <Text>Date: {new Date().toDateString()}</Text>
+      <View style={[styles.defaultMargin, styles.contactInfo]}>
+        <Text style={styles.text}>Faculty Name: {props.faculty}</Text>
+        <Text style={styles.text}>Contact: {props.contact}</Text>
+        <Text style={styles.text}>Date: {new Date().toDateString()}</Text>
       </View>
-      <View>
-        <Text>
+      <View style={styles.defaultMargin}>
+        <Text style={styles.text}>
           Original Request: {props.orderedType} {props.orderedItem},{" "}
           {props.orderedQuantity}
         </Text>
       </View>
-      <View>
-        <Text>Items ordered:</Text>
+      <View style={styles.defaultMargin}>
+        <Text style={styles.text}>Items ordered:</Text>
         {props.items.map((v, k) => {
-          return <Text>ID: {v.id}</Text>;
+          return <Text style={styles.text}>ID: {v.id}</Text>;
         })}
       </View>
-      <View>
-        <Text>Total Price: ${props.price}</Text>
+      <View style={[styles.defaultMargin, styles.finalPriceContainer]}>
+        <Text style={[styles.text, styles.finalPrice]}>Total Price: ${props.price}</Text>
       </View>
     </Page>
   </Document>
