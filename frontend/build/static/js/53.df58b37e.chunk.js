f2282f5c2aac671278d28bf52c9289143c05b761
Ratik Kapoor <ratikrk@gmail.com>
2021-04-12 12:10:51 -0600

    chore: Add react production build

---
+++ frontend/build/static/js/53.df58b37e.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreinventorii=this.webpackJsonpreinventorii||[]).push([[53],{549:function(n,t,e){"use strict";e.r(t),e.d(t,"startStatusTap",(function(){return r}));var i=e(23),o=e(20),r=function(){var n=window;n.addEventListener("statusTap",(function(){Object(i.h)((function(){var t=n.innerWidth,e=n.innerHeight,r=document.elementFromPoint(t/2,e/2);if(r){var c=r.closest("ion-content");c&&new Promise((function(n){return Object(o.c)(c,n)})).then((function(){Object(i.f)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
+//# sourceMappingURL=53.df58b37e.chunk.js.map
