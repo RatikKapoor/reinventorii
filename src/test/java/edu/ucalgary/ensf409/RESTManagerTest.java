7f9e7a55d2c955a07f1b7fdfb01550f21583fd14
Risath18 <risathq@gmail.com>
2021-04-13 15:49:20 -0600

    Fix: Cleaned up Errors in DatabaseTest

--- src/test/java/edu/ucalgary/ensf409/RESTManagerTest.java
+++ src/test/java/edu/ucalgary/ensf409/RESTManagerTest.java
@@ -5,6 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.Assert.*;
 
+/**
+ * Tests for RESTManagerTest Class. Checking constructors. Cannot create unit
+ * tests for other methods since they are end-to-end tests between frontend and
+ * backend.
+ * 
+ * @version 1.1
+ */
 @SpringBootTest
 public class RESTManagerTest {
     @Test
