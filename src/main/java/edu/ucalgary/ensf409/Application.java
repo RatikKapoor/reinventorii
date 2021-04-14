53f88aea5b345d29b1bcc70e005a7362792cccf1
anandp1 <73033746+anandp1@users.noreply.github.com>
2021-04-14 13:44:50 -0600

    Added Documentation

--- src/main/java/edu/ucalgary/ensf409/Application.java
+++ src/main/java/edu/ucalgary/ensf409/Application.java
@@ -3,6 +3,12 @@ package edu.ucalgary.ensf409;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Class Application to run SpringBoot
+ * 
+ * @author Anand Patel, Robert Brown, Ratik Kapoor, Risat Haque
+ * @since 1.0
+ */
 @SpringBootApplication
 public class Application {
 
--- src/test/java/edu/ucalgary/ensf409/ApplicationTests.java
+++ src/test/java/edu/ucalgary/ensf409/ApplicationTests.java
@@ -3,6 +3,12 @@ package edu.ucalgary.ensf409;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * Class ApplicationTests to run tests
+ * 
+ * @author Anand Patel, Robert Brown, Ratik Kapoor, Risat Haque
+ * @since 1.0
+ */
 @SpringBootTest
 class ApplicationTests {
 
